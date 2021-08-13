b27336b9bc29b7a5ce055fe2059c4a44aa5644cc
Ricards Taujenis <MozesThegreat@yahoo.com>
2021-08-13 17:46:07 +0200

    Header created btc image attached

--- vue-crypto-search/src/components/Header.vue
+++ vue-crypto-search/src/components/Header.vue
@@ -1,9 +1,7 @@
 <template>
   <header class="text-center bg-blue-400 text-white p-4 mb-10">
-    
     <img src="@/assets/btc.png" alt="bitcoin" class="object-contain h-20" >
     <div class="text-4xl italic md:not-italic align-text-top">Crypto Searcher</div>
-   
   </header>
 </template>
 
