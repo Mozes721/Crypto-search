bb74f289171f0b8b57126f62d0a023733c64ee90
Mozes721 <Richard.Taujenis@gmail.com>
2021-08-20 17:08:48 +0200

    crypto value displayed

---
+++ vue-crypto-search/src/components/CryptoCoins.vue
@@ -0,0 +1,21 @@
+<template>
+ <div class="grid grid-cols-1 gap-3">
+    <div class="shadow-md bg-blue-200 p-10 text-center rounded">
+    <h3>Coin</h3>
+    {{ cryptodata[1].currency }}
+    {{ cryptodata[1].price }}
+    {{ cryptodata[1].circulating_supply }}
+    {{ cryptodata[1].market_cap }}
+    </div> 
+ </div>
+</template>
+
+<script>
+
+
+export default {
+ name: 'CryptoCoins',
+ props: ['cryptodata'],
+}
+</script>
+
--- vue-crypto-search/src/views/Home.vue
+++ vue-crypto-search/src/views/Home.vue
@@ -1,20 +1,31 @@
 <template>
-  <div class="home">
-    Hell Crypto
-    
+  <main v-if="!loading">
+    <CryptoCoins :cryptodata="cryptodata" />
+  </main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+  <div class="text-gray-600 text-4xl mt-9 mb-5">
+    Fetching Data
   </div>
+  <img :src="loadingImage" class="w-30 m-auto" alt="" />
+  </main>
 </template>
 
 <script>
+import CryptoCoins from '@/components/CryptoCoins'
+
 export default {
   name: 'Home',
-  components: {},
+  components: {
+    CryptoCoins
+  },
   data() {
       return {
-        loading: true,
+        info: '',
+        // loading: false,
+        cryptodata: [],
         currency: 'BTC',
-        logo_url : '',
-        
+   
+        loadingImage: require('../assets/super-buu-hourglass.gif')
       }
     },
   methods: {
@@ -27,8 +38,14 @@ export default {
   },
     async created() {
       const data = await this.fetchCryptoData()
-      console.log(data)
-      .then(response => (this.crypto = response))
+      console.log(data[0].currency)
+      this.cryptodata = data
+      this.currency = data.currency
+      this.logo_url = data.logo_url
+      this.stats = data.id
+      this.price = data.price
+      this.circulating_supply = data.circulating_supply
+      this.market_cap = data.market_cap
     },
 }
 
