b71fd27289c4accfdbbfe44c2d840b90aafacbfc
Mozes721 <Richard.Taujenis@gmail.com>
2021-08-24 17:23:53 +0200

    search all crypto names added

---
+++ vue-crypto-search/src/components/CryptoSelect.vue
@@ -0,0 +1,18 @@
+<template>
+<select v-model="selected" class="form-select block w-full border p-3 rounded mt-10 bg-blue-50">
+	<option value="0">Select your coin</option>
+	<option v-for="crypto in cryptodata" :value="crypto.ID">{{crypto.name}}</option>
+</select>
+</template>
+
+<script>
+export default {
+    name: 'CryptoSelect',
+    props: ['cryptodata'],
+    data () {
+        return {
+	selected: 0
+	}
+    }
+}
+</script>
--- vue-crypto-search/src/views/Home.vue
+++ vue-crypto-search/src/views/Home.vue
@@ -1,48 +1,56 @@
 <template>
   <main v-if="!loading">
-    <CryptoCoins :cryptodata="cryptodata" />
+    <CryptoCoin :cryptodata="cryptodata" />
+  
+    <CryptoSelect :cryptodata="cryptodata"/>
   </main>
+
   <main class="flex flex-col align-center justify-center text-center" v-else>
-  <div class="text-gray-600 text-4xl mt-9 mb-5">
-    Fetching Data
-  </div>
-  <img :src="loadingImage" class="w-30 m-auto" alt="" />
+    <div class="text-gray-600 text-4xl mt-9 mb-5">
+      Fetching Data
+    </div>
+    <img :src="loadingImage" class="w-30 m-auto" alt="searching" />
   </main>
 </template>
 
 <script>
-import CryptoCoins from '@/components/CryptoCoins'
- 
+import CryptoCoin from '@/components/CryptoCoin'
+import CryptoSelect from '@/components/CryptoSelect'
+
 export default {
   name: 'Home',
   components: {
-    CryptoCoins
+    CryptoCoin,
+    CryptoSelect
   },
   data() {
       return {
         loading: false,
+        name: 'Bitcoin',
         cryptodata: [],
+        // cryptoSearched: [],
         loadingImage: require('../assets/super-buu-hourglass.gif')
       }
     },
   methods: {
     async fetchCryptoData() {
-    
+
       const res = await fetch('https://api.nomics.com/v1/currencies/ticker?key=ba659a90cd165d860bb631048dd09e1fab4a2f47')
       const data = await res.json()
       return data
-    }
+    
+    },
   },
     async created() {
       const data = await this.fetchCryptoData()
-      console.log(data[0].currency)
       this.cryptodata = data
-      this.currency = data.currency
-      this.logo_url = data.logo_url
-      this.stats = data.id
-      this.price = data.price
-      this.circulating_supply = data.circulating_supply
-      this.market_cap = data.market_cap
+      // this.cryptoSearched = data
+      // this.currency = data.currency
+      // this.logo_url = data.logo_url
+      // this.stats = data.id
+      // this.price = data.price
+      // this.circulating_supply = data.circulating_supply
+      // this.market_cap = data.market_cap
     },
 }
 
